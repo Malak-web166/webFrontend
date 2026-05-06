db673924157910759afc94044d511971735fb99f
Malak-web166 <m.mousa0499@student.aast.edu>
2026-05-06 03:52:01 +0300

    Delete Frontend directory

--- Frontend/src/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'http://localhost:5118/api', // Double check your backend port!
-  withCredentials: true, // MUST be true for your backend cookies to work
-});
-
-export default api;
